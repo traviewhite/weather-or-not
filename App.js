5204be7a870fb6ae242d0cb17fc0612084cd1b0a
Travis White <traviewhite@gmail.com>
2024-04-24 14:36:43 -0500

    feat: update app.js w title

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={{ color: 'white', fontSize: 34, fontWeight: 800 }}>WeatherOrNot</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(53, 34, 77)',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
