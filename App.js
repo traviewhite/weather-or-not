4646ee10e8da38c938a07a00a351a579effa0478
Travis White <traviewhite@gmail.com>
2024-04-24 14:37:55 -0500

    feat: add linear-gradient background

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text style={{ color: 'white', fontSize: 34, fontWeight: 800 }}>WeatherOrNot</Text>
+      <LinearGradient colors={['rgba(93, 56, 140, 0.5)', 'transparent']} style={styles.background}></LinearGradient>
     </View>
   )
 }
@@ -16,5 +18,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgb(53, 34, 77)',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
   },
 })
